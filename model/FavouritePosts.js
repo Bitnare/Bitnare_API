b8c31c2e01faf43f9185e4beb84cb5a93f4bb565
poonamlama <poonamlama253@gmail.com>
2020-03-04 16:39:45 +0545

    get favourite post complete

--- model/FavouritePosts.js
+++ model/FavouritePosts.js
@@ -5,7 +5,7 @@ const Schema = mongoose.Schema;
 const favouriteSchema = new Schema({
     post_id:{
         type: Schema.Types.ObjectId,
-        ref: 'BitPosts'
+        ref: 'BitPosts',
     },
     userid: {
         type: Schema.Types.ObjectId,
--- routes/favouritePosts.js
+++ routes/favouritePosts.js
@@ -3,29 +3,31 @@ const router = express.Router();
 const Post = require("../model/FavouritePosts");
 const auth = require('../middleware/verifytoken.js');
 
+
 //add favourites
 router.post("/addFavourite/:id", auth, function(req,res){
     const userid = req.user._id;
     const post_id=req.params.id.toString();
     const favPost = new Post ({post_id:post_id,userid:userid});
     favPost.save()
-           .then(function(favpost){
-               res.send(favpost) ;
-           }).catch(function(e){
-                res.send(e);
-           })
+    .then(function(favpost){
+        res.send(favpost);
+    }).catch(function(e){
+        res.send(e);
+    })
 });
 
 //get all favourites
 router.get('/getFavourite', function(req, res) {
-    const favPost = 
     Post.find()
-            .select("-__v")
-            .then(function(favPost) {
-                res.send(favPost);
-            }).catch(function(e) {
-                res.send(e);
-            });
+    .select("-__v")
+    .populate('userid', 'username')
+    .populate('post_id')
+    .then(function(favPost) {
+        res.send(favPost);
+    }).catch(function(e) {
+        res.send(e);
+    });
 });
 
 //delete favourite by id

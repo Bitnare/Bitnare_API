0d51edde5e939b66875edbb313adab50f9c1ffd3
bibav777 <you@example.com>
2020-03-05 03:16:23 -0800

    middleware updated for comment

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const userProfile = require("./routes/userProfile");
 const adminLogin = require("./routes/adminProfile");
 const userPost = require("./routes/userPosts.js");
 const favouritePost = require("./routes/favouritePosts");
-
+const postComment = require("./routes/postComment");
 const follow = require("./routes/follow");
 
 
@@ -23,9 +23,11 @@ app.use(cors());
 app.use("/user", userProfile);
 app.use("/post", userPost);
 
-app.use("/user",follow);
+app.use("/user", follow);
+app.use("/comment", postComment);
 
 app.use("/favourites", favouritePost);
+
 
 
 
